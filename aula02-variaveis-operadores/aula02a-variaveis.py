10d90adc1f2a921c46d7b56db3d9293e5700848f
alexandrerussi <alexandrerussijunior@gmail.com>
2026-03-16 09:53:14 -0300

    aula 02 - variaveis

---
+++ aula02-variaveis-operadores/aula02a-variaveis.py
@@ -0,0 +1,32 @@
+print("Olá, mundo!")
+print(7 + 4)
+print('7' + '4') # CONCATENANDO STRINGS
+
+# Comentários em python utilizo a #
+'''
+    Comentários
+    de múltiplas
+    linhas
+'''
+
+nome = "Alexandre"
+idade = 26
+peso = 70.2
+
+print(nome, idade, peso)
+
+# INPUT - UTILIZADO PARA RECEBER DADOS DO USUÁRIO
+
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+peso = input("Digite seu peso: ")
+
+print(nome, "oiiii", peso)
+print(f"Oii, {nome}")
+
+#
+
+
+
+
+
